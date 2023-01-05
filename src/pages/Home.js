8345d38f87d91e92245f3b23733369724c33c234
Brandon Tobin <brandontobinbusiness@gmail.com>
2023-01-05 15:36:06 -0600

    grabbing from dev

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import BannerImage from "../assets/bannerimage.jpeg"
+import BannerImage from "../../assets"
 import "../styles/Home.css";
 
 function Home() {
