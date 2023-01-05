1f46dc24581dc5220ec01695e01abb37c3113359
Brandon Tobin <brandontobinbusiness@gmail.com>
2023-01-05 10:59:11 -0600

    changes to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
 function Home(props) {
   return (
     <div className="home">
-      <h1> Home </h1>
+      <img
+        className="homepage-image"
+        src="https://www.gosportz.in/images/inside-banner.jpg"
+        alt=""
+      />
     </div>
   );
 }
