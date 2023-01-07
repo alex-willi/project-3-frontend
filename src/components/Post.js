6be3f486efb9b3b0fa05e8c40a389b89a0e33eab
Brandon Tobin <brandontobinbusiness@gmail.com>
2023-01-06 20:57:06 -0600

    pushing up to dev

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,27 +1,24 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router";
+
 function Posts(props) {
   const [posts, setPosts] = useState();
   const { id } = useParams();
-  const URL = `http://localhost:4000/authors/${id}`;
+  const URL = `http://localhost:4000/posts/${id}`;
   useEffect(() => {
     fetch(URL)
       .then((response) => response.json())
       .then((json) => {
         setPosts(json);
+        console.log(json);
       })
       .catch(console.err);
   }, [URL]);
 
-  return posts.map((post) => {
-    return (
-      <div className="posts">
-        <h1> {post.title} </h1>
-        <img src={post.photo} alt="sports"></img>
-        <h1> {post.body} </h1>
-      </div>
-    );
-  });
-}
 
+
+  return (
+    <h1>hi</h1>
+  )
+}
 export default Posts;
