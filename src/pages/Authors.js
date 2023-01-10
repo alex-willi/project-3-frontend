04a1319b1ed683b6c1da34cfd5cfadc2ea20478a
cagdaskalsen <cagdaskalsen@hotmail.com>
2023-01-10 16:00:57 -0500

    Pics added

--- src/pages/Authors.js
+++ src/pages/Authors.js
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import "../styles/Feed.css";
+// import brandon from "../assets/brandontobin.jpeg";
+// import alex from "../assets/alexwilliams.jpeg";
+// import cagdas from "../assets/cagdaskalsen.jpeg";
+
 function Authors(props) {
   const [authors, setAuthors] = useState(null);
   const BASE_URL = "https://project-3-sports.herokuapp.com/";
@@ -9,7 +12,7 @@ function Authors(props) {
       return (
         <div key={index} className="authors">
           <Link className="authorslink" to={`/authors/${author._id}`}>
-            <h1> {author.name} </h1>{" "}
+            <h1>{author.name} </h1>
           </Link>
         </div>
       );
@@ -17,7 +20,7 @@ function Authors(props) {
   };
   const loading = () => (
     <section className="user-list">
-      <h1> Loading... </h1>{" "}
+      <h1> Loading... </h1>
     </section>
   );
   async function fetchUser() {
@@ -35,32 +38,12 @@ function Authors(props) {
   }, []);
 
   return (
-    <div id="authorsinfo">
-      <section className="user-list">
-        {" "}
-        {authors && authors.length ? loaded() : loading()}{" "}
-      </section>
-      <div id="pics">
-        <div id="pic-alex">
-          <img
-            src="https://media.licdn.com/dms/image/D5603AQHaf1AATzG9EA/profile-displayphoto-shrink_200_200/0/1669903290075?e=1678924800&v=beta&t=j9tKMS7siwoxmWqliC4lMSog_8fZsRiC7UpwdaWlTSw"
-            alt=""
-          />
-        </div>
-        <div id="pic-cagdas">
-          <img
-            src="https://media.licdn.com/dms/image/D4E03AQGugarVN3zn2g/profile-displayphoto-shrink_200_200/0/1672529138321?e=1678924800&v=beta&t=AXVGiSo6vWFIJG3c34ARCK4AZs-6_ffid45IHk3kxVY"
-            alt=""
-          />
-        </div>
-        <div id="pic-brandon">
-          <img
-            src="https://media.licdn.com/dms/image/D4E03AQGsGIMyNwxk0Q/profile-displayphoto-shrink_200_200/0/1631978161970?e=1678924800&v=beta&t=zrDDRgQmyfNp96a7oFafT4hJEt4Gp6c5rBvhb40zjqw"
-            alt=""
-          />
-        </div>
-      </div>
-    </div>
+    <section className="user-list">
+      {authors && authors.length ? loaded() : loading()}
+      {/* <img className="alex-image" src={alex} />
+      <img className="cagdas-image" src={cagdas} />
+      <img className="brandon-image" src={brandon} /> */}
+    </section>
   );
 }
 
