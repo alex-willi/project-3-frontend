4e9fe3dd775648e7fc2579b3bee2ab9e2c50f2a8
cagdaskalsen <cagdaskalsen@hotmail.com>
2023-01-10 21:30:19 -0500

    More CSS

--- src/components/AuthorPost.js
+++ src/components/AuthorPost.js
@@ -3,38 +3,29 @@ import "../styles/Authors.css";
 import { Link } from "react-router-dom";
 
 function AuthorPost(props) {
-  // destructure `post` property from `props` object
   const { post } = props;
+  const { author } = props;
 
-  // check if the post data is present
   if (post) {
     return (
       <>
-        {props.onDelete && (
-          <button id="delete-btn" onClick={() => props.onDelete(post._id)}>
-            Delete Post
-          </button>
-        )}
+        <Link to={`/feed/`}>
+          {props.onDelete && (
+            <button onClick={() => props.onDelete(post._id)}>
+              Delete Post
+            </button>
+          )}
+        </Link>
         <div id="allposts">
-          {/* create a link to navigate to the post details page */}
+          <h1>{author}</h1>
           <Link key={post.id} className="postlink" to={`/posts/${post._id}`}>
-            <h1 className="post-title">
-              {post.title}
-              {/* render the delete button if onDelete prop is passed */}
-            </h1>
+            <h1 className="post-title">{post.title}</h1>
             <img className="feed-image" src={post.photo} alt="sports"></img>
             <h1>{post.body}</h1>
           </Link>
-
-          {props.author ? (
-            <h1 className="author-name">Author: {props.author.name}</h1>
-          ) : (
-            ""
-          )}
         </div>
       </>
     );
   }
 }
-
 export default AuthorPost;
