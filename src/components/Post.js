3cade7f7b80e441d25e378cedae0d017a6987d73
alex-willi <alex.lawson.williams@gmail.com>
2023-01-10 21:52:48 -0600

    deploy

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,15 +4,9 @@ import "../styles/Feed.css";
 function Post(props) {
   if (props.post) {
     return (
-<<<<<<< HEAD
-      <div id="all-posts">
-        <h1 className="posttitle">{props.post.title}</h1>
-        <img className="feedimage" src={props.post.photo} alt="sports"></img>
-=======
       <div id="allposts">
         <h1 className="post-title">{props.post.title}</h1>
         <img className="feed-image" src={props.post.photo} alt="sports"></img>
->>>>>>> dev
         {props.author ? (
           <h1 className="author-name"> Author: {props.author.name} </h1>
         ) : (
